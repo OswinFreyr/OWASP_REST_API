57e2fc04e550e6c9e4e21ea31b76998997476af5
OswinFreyr <pisanotanguy@gmail.com>
2025-03-12 15:22:38 +0100

    ajout associations et initialisation db et app de l'api

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+// const cors = require("cors");
+const { db } = require("./models/db");
+
+const pokemonRouter = require("./routes/pokemonRoute");
+const userRouter = require("./routes/userRoute");
+
+const app = express();
+const PORT = 2000;
+// app.use(cors({
+//     origin: 'http://localhost:4000', 
+//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
+//     allowedHeaders: ['Content-Type'],
+// }));
+
+app.use(express.json({limit: "2mb"}));
+
+app.use("/api/v1/pokemons",pokemonRouter);
+app.use("/api/v1/users",userRouter);
+
+
+db.sync(/*{force : true}*/)
+    .then(async () => {
+            app.listen(PORT, () => {
+            console.log(`http://localhost:${PORT}`);
+        })
+    });
